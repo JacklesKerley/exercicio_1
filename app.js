0bbdffe1a2a1b9231191e84f946fe62aa42ea0b1
Jackles kerley <jackleskerley@yahoo.com.br>
2023-03-20 14:32:16 -0300

    validação de idade

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+
+const app = express()
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Rota
+app.get('/', (req, res) => {
+    res.render('verificacaoIdade')
+})
+
+app.post('/', (req, res) => {
+    const nome = req.body.nome
+    const idade = req.body.idade
+    let idadeValidada = ''
+
+    if (idade >= 0 && idade < 15) {
+        idadeValidada = 'Criança'
+    } else if (idade >= 15 && idade < 30) {
+        idadeValidada = 'Jovem'
+    } else if (idade >= 30 && idade < 60) {
+        idadeValidada = 'Adulto'
+    } else {
+        idadeValidada = 'Idoso'
+    }
+})
+
+app.listen(3000, () => {console.log('App Rodando')})
